22af49193e486127f52f0cf9829f03aabf3828a7
James Madden <jamesbmadden@gmail.com>
2022-07-21 12:42:22 -0700

    Create cpu.rs

---
+++ src/cpu.rs
@@ -0,0 +1,14 @@
+/**
+ * This struct reads and interprets instructions, handling memory and connecting with
+ * the display and audio outputs as well as the keyboard inputs
+ */
+
+pub struct Cpu {
+
+}
+
+impl Cpu {
+
+  
+
+}
