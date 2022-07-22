ec8a37adcb446abafc3a10005faa35d2b17e371e
James Madden <jamesbmadden@gmail.com>
2022-07-21 17:02:47 -0700

    Add keyboard state functionality

--- src/keyboard.rs
+++ src/keyboard.rs
@@ -1,13 +1,14 @@
 /**
  * Handles user input through the keyboard.
  */
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use winit::event::VirtualKeyCode;
 
 pub struct Keyboard {
 
   // map of keys from wgpu to numbers for the instructions to process
-  KEY_MAP: HashMap<VirtualKeyCode, u8>
+  pub key_map: HashMap<VirtualKeyCode, u8>,
+  pub keys_down: HashSet<u8>
 
 }
 
@@ -17,7 +18,7 @@ impl Keyboard {
 
     // create the key map
     // this maps the modern key arrangement to the traditional chip-8 key codes
-    let KEY_MAP: HashMap<VirtualKeyCode, u8> = HashMap::from([
+    let key_map: HashMap<VirtualKeyCode, u8> = HashMap::from([
       (VirtualKeyCode::Key1,  0x1),
       (VirtualKeyCode::Key2,  0x2),
       (VirtualKeyCode::Key3,  0x3),
@@ -36,7 +37,52 @@ impl Keyboard {
       (VirtualKeyCode::V,     0xF)
     ]);
 
-    return Keyboard { KEY_MAP };
+    // the list of which keys are currently down
+    let keys_down: HashSet<u8> = HashSet::new();
+
+    return Keyboard { key_map, keys_down };
+
+  }
+
+  /**
+   * Check whether keys_down contains the requested key
+   */
+  pub fn is_key_pressed(&self, key_code: u8) -> bool {
+
+    // just check if key_code is in the set
+    return self.keys_down.contains(&key_code);
+
+  }
+
+  /**
+   * A key is pressed! Add it to the set and potentially run next key press
+   */
+  pub fn on_key_down(&mut self, key: VirtualKeyCode) {
+
+    // first, find the chip-8 key code
+    let key_code = self.key_map.get(&key);
+
+    // if key_code is None, it's not one of our wanted keys so we should return now
+    if key_code == None { return };
+
+    // now add the pressed key to the pressed key set
+    self.keys_down.insert(*key_code.unwrap());
+
+  }
+
+  /**
+   * The key is up, remove it from the set
+   */
+  pub fn on_key_up(&mut self, key: VirtualKeyCode) {
+
+    // first, find the chip-8 key code
+    let key_code = self.key_map.get(&key);
+
+    // if key_code is None, it's not one of our wanted keys so we should return now
+    if key_code == None { return };
+
+    // now remove the pressed key frpm the pressed key set
+    self.keys_down.remove(key_code.unwrap());
 
   }
 
