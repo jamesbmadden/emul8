7805ad6edbc363b960b53769ca26279b510cc6f0
James Madden <jamesbmadden@gmail.com>
2022-07-21 19:42:36 -0700

    Start adding cpu instructions

--- src/cpu.rs
+++ src/cpu.rs
@@ -26,7 +26,7 @@ pub struct Cpu {
   pub paused: bool,
   pub speed: u16,
 
-  pub stack: Vec<u16>
+  pub stack: Vec<usize>
 
 }
 
@@ -51,9 +51,9 @@ impl Cpu {
     let sound_timer: u16 = 0;
 
     // address in the program
-    let program_addr: usize = 0;
+    let program_addr: usize = 0x200;
 
-    let stack: Vec<u16> = vec![];
+    let stack: Vec<usize> = vec![];
 
     // state for how the game is running
     let speed: u16 = 10;
@@ -123,10 +123,12 @@ impl Cpu {
     if !self.paused {
 
       // run however many instructions are specified in the speed variable
-      for i in 0..self.speed {
+      for _i in 0..self.speed {
 
           // figure out the operation we're running
-          let opcode = (self.memory[self.program_addr] as u16) << 8 | self.memory[self.program_addr + 1] as u16;
+          let instruction = (self.memory[self.program_addr] as u16) << 8 | self.memory[self.program_addr + 1] as u16;
+          // execute the instruction
+          self.execute_instruction(instruction);
 
       }
 
@@ -153,6 +155,121 @@ impl Cpu {
     }
     if self.sound_timer > 0 {
       self.sound_timer -= 1;
+    }
+
+  }
+
+  /**
+   * The big one: take in an instruction code and determine what to do
+   */
+  pub fn execute_instruction(&mut self, instruction: u16) {
+
+    // update program address so the next instruction is run
+    self.program_addr += 2;
+
+    // x and y values, located at 0xy0 in the instruction, are used in
+    // many different instructions, so they should be grabbed now to
+    // reduce repetition of code
+    // use the & operation to isolate only the specific 4 bits we want
+    let x = ((instruction & 0x0F00) >> 8) as usize;
+    let y = ((instruction & 0x00F0) >> 4) as usize;
+
+    // match different instructions, starting off with the greatest value (the first 4 bits)
+    // and then narrowing down depending on the instruction
+    // instructions can be referenced in the technical specification, simple
+    // explainers will be included in comments here
+    match instruction & 0xF000 {
+
+      // there's two options for what a 0x0 instruction could be
+      0x0000 => match instruction {
+
+        // clear the screen
+        0x00E0 => self.display.clear(),
+        // exit a subroutine by setting the program counter to the top of the stack
+        0x00EE => self.program_addr = self.stack.pop().unwrap() as usize,
+        // nothing else is real so it can be safely ignored
+        _ => ()
+
+      }
+
+      // jump to address nnn where nnn is the last 12 bits in the instruction
+      0x1000 => self.program_addr = (instruction & 0x0FFF) as usize,
+
+      // add the current address to the stack and start a subroutine at the last 12 bits
+      0x2000 => {
+        self.stack.push(self.program_addr);
+        self.program_addr = (instruction & 0x0FFF) as usize;
+      },
+
+      // if the value of v[x] equals the second byte, skip the next instruction
+      0x3000 => if self.v[x] as u16 == instruction & 0x00FF {
+        self.program_addr += 2;
+      },
+
+      // if the value of v[x] DOESN'T equal the second byte, skip the next instruction
+      0x4000 => if self.v[x] as u16 != instruction & 0x00FF {
+        self.program_addr += 2;
+      },
+
+      // if v[x] equals v[y], skip the next instruction
+      0x5000 => if self.v[x] == self.v[y] {
+        self.program_addr += 2;
+      },
+
+      // set v[x] to the second byte
+      0x6000 => self.v[x] = (instruction & 0x00FF) as u8,
+
+      // add the second byte to v[x]
+      0x7000 => self.v[x] += (instruction & 0x00FF) as u8,
+
+
+      // 0x8000 series performs operations on the contents of the v store
+      // all use x and y values, specify different operation using the last
+      // four bits
+      0x8000 => match instruction & 0xF {
+
+        // sets store x to the value of store y
+        0x0 => self.v[x] = self.v[y],
+        // store bitwise OR on v[x] and v[y] in v[x]
+        0x1 => self.v[x] = self.v[x] | self.v[y],
+        // store bitwise AND on v[x] and v[y] in v[x]
+        0x2 => self.v[x] = self.v[x] & self.v[y],
+        // store bitwise XOR on v[x] and v[y] in v[x]
+        0x3 => self.v[x] = self.v[x] ^ self.v[y],
+        // add v[x] and v[y] together, storing extra bit in v[0xF]
+        0x4 => {
+          // add them together
+          let sum = self.v[x] as u16 + self.v[y] as u16;
+          // set v[15] to whether or not sum is greater than the max
+          self.v[15] = (sum > 255) as u8;
+          // set v[x] to the sum, cutting off any potential overflow
+          self.v[x] = (sum & 0xFF) as u8;
+        },
+        // v[y] is subtracted from v[x]. v[15] = v[y] > v[x]
+        0x5 => {
+          self.v[15] = (self.v[x] > self.v[y]) as u8;
+          self.v[x] = self.v[x] - self.v[y];
+        },
+        // divide v[x] by 2, and set v[15] to the most significant bit of v[x]
+        0x6 => {
+          // if v[x] is >= 128, the 8th bit must be 1
+          self.v[15] = (self.v[x] >= 128) as u8;
+          self.v[x] /= 2;
+        },
+        // v[x] is subtracted from v[y]. v[15] = v[x] > v[y]
+        0x7 => {
+          self.v[15] = (self.v[y] > self.v[x]) as u8;
+          self.v[x] = self.v[y] - self.v[x];
+        },
+
+        // no other options
+        _ => (),
+
+      },
+
+      // if any instruction is encountered that isn't yet implemented, give a todo
+      _ => todo!()
+
     }
 
   }
